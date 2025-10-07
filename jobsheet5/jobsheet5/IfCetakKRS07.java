f8c46f01da775acc19d68a5d3eb7cc5d16859650
sesilia-acc <dandinsesilia@gmail.com>
2025-10-06 20:21:44 -0700

    Percobaan 1

---
+++ jobsheet5/jobsheet5/IfCetakKRS07.java
@@ -0,0 +1,16 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class IfCetakKRS07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----CETAK KRS SIAKAD----");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        String hasil = (uktLunas) ? "Pembayaran UKT terverifikasi \n Silakan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak. Silakan membayar UKT terlebih dahulu";
+        System.out.println(hasil);
+    }
+}
