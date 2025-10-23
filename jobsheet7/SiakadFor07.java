1b88ed77c7c3d2a19f44f67e05d55e31be7d34aa
sesilia-acc <dandinsesilia@gmail.com>
2025-10-23 07:24:08 -0700

    Modifikasi percobaan 1

--- jobsheet7/SiakadFor07.java
+++ jobsheet7/SiakadFor07.java
@@ -7,6 +7,7 @@ public class SiakadFor07 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke-" + i + ": ");
@@ -17,8 +18,16 @@ public class SiakadFor07 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai <= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah : " + terendah);
+
+        System.out.println("Mahasiswa lulus : " + lulus);
+        System.out.println("Mahasiswa tidak lulus : " + tidakLulus);
     }
 }
