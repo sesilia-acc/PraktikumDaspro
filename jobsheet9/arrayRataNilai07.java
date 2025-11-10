a5fc5ef7091190de4ad0c9faf791218ad5b61aeb
sesilia-acc <dandinsesilia@gmail.com>
2025-11-10 08:33:27 -0800

    Percobaan 3

---
+++ jobsheet9/arrayRataNilai07.java
@@ -0,0 +1,23 @@
+package jobsheet9;
+
+import java.util.Scanner;
+
+public class arrayRataNilai07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiMhs = new int[10];
+        double total = 0, rerata; 
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+        rerata = total / nilaiMhs.length;
+        System.out.println("Rata - rata nilai = " + rerata);
+    }
+}
