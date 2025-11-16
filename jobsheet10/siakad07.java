3c1e3acb7a41d8c37bba81ae93cf01a3959d5173
sesilia-acc <dandinsesilia@gmail.com>
2025-11-16 13:25:19 -0800

    Modifikasi percobaan 4

--- jobsheet10/siakad07.java
+++ jobsheet10/siakad07.java
@@ -6,20 +6,15 @@ public class siakad07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah mata kuliah yang akan diinput: ");
+        int jmlMK = sc.nextInt();
+        System.out.print("Masukkan jumlah Mahasiswa yang akan diinput: ");
+        int jmlMhs = sc.nextInt();
 
-    /*    for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-
-            for (int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
-                nilai[i][j] = sc.nextInt();
-            }
-        }
-    */
+        int[][] nilai = new int[jmlMK][jmlMhs];
 
         for (int i = 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
             for (int j = 0; j < nilai[i].length; j++) {
@@ -27,7 +22,7 @@ public class siakad07 {
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata - rata: " + totalPerSiswa / 3);
+            System.out.println("Nilai rata - rata: " + totalPerSiswa / jmlMhs);
         }
         System.out.println("\n======================================");
         System.out.println("Rata - rata nilai setiap mata kuliah: ");
@@ -38,7 +33,7 @@ public class siakad07 {
             for (int i = 0; i < nilai.length; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / jmlMK);
         }
         
         sc.close();
