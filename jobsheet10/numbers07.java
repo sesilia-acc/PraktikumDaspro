6233efbd19a771efae70d5c987149456e9e33a61
sesilia-acc <dandinsesilia@gmail.com>
2025-11-15 19:38:52 -0800

    Modifikasi percobaan 3

--- jobsheet10/numbers07.java
+++ jobsheet10/numbers07.java
@@ -1,11 +1,21 @@
 package jobsheet10;
 
+import java.util.Arrays;
+
 public class numbers07 {
     public static void main(String[] args) {
-        
+
         int[][] myNumbers = new int[3][];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        System.out.println();
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
+        }
     }
 }
