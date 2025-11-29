b1fe3548815e27be0198c32e9c3c3a391c00324a
sesilia-acc <dandinsesilia@gmail.com>
2025-11-28 19:27:04 -0800

    Percobaan 1

---
+++ jobsheet11/kafe07.java
@@ -0,0 +1,18 @@
+package jobsheet11;
+
+public class kafe07 {
+    public static void Menu() {
+        System.out.println("====MENU RESTO KAFE====");
+        System.out.println("1. Kopi Hitam - Rp15,000");
+        System.out.println("2. Cappuccino - Rp20,000");
+        System.out.println("3. Latte - Rp22,000");
+        System.out.println("4. Teh Tarik - Rp12,000");
+        System.out.println("5. Roti Bakar - Rp10,000");
+        System.out.println("6. Mie Goreng - Rp18,000");
+        System.out.println("===========================");
+        System.out.println("SIlakan pilih menu yang Anda inginkan.");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
