c9711051ed7a7f9a26720848a31b8cbdc989d1c7
sesilia-acc <dandinsesilia@gmail.com>
2025-12-02 22:10:33 -0800

    Modifikasi percobaan 4

--- jobsheet11/pengunjungKafe07.java
+++ jobsheet11/pengunjungKafe07.java
@@ -3,13 +3,11 @@ package jobsheet11;
 public class pengunjungKafe07 {
     public static void daftarPengunjung(String...namaPengunjung) {
         System.out.println("Daftar nama pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String pengunjung : namaPengunjung) {
+            System.out.println("- " + pengunjung);
         }
     }
     public static void main(String[] args) {
-        daftarPengunjung("Ali", "Budi", "Citra");
-        daftarPengunjung("Andi");
-        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+        daftarPengunjung();
     }
 }
